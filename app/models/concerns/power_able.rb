66e670a94ffbc1f1df7a1267d18ad3c9728f6659
Weirong Xu <weirongxu.raidou@gmail.com>
2018-07-14 14:44:48 +0800

    add date_or_range2where method

--- app/models/concerns/power_able.rb
+++ app/models/concerns/power_able.rb
@@ -19,6 +19,29 @@ module PowerAble
         end
       end
     end
+
+    def self.date_or_range2where(column, datetime)
+      if datetime.is_a? Range
+        conds = []
+        if not datetime.first.try(:infinite?)
+          conds.push("#{column} >= '#{datetime.first.to_s(:db)}'")
+        elsif not datetime.last.try(:infinite?)
+          if exclude_end?
+            conds.push("#{column} < '#{datetime.last.to_s(:db)}'")
+          else
+            conds.push("#{column} <= '#{datetime.last.to_s(:db)}'")
+          end
+        end
+        sql = conds.join(' AND ')
+        if sql.empty?
+          '1=1'
+        else
+          sql
+        end
+      else
+        "#{column} = '#{datetime.to_s(:db)}'"
+      end
+    end
   end
 
   def serializer(options={})
